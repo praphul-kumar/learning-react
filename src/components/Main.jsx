f9a087c751f65e71138cb7b07717356870e17b95
Praphul Kumar <praphulkumar2023@gmail.com>
2024-03-26 13:26:05 +0530

    cleaning dynamic Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,9 @@ function genRandomInt(max) {
 }
 
 export default function Main() {
+  // We can also do this way to make the component little cleaner
+  const description = reactDescription[genRandomInt(2)];
+
   return (
     <div className="main">
       <img src={reactLogo} alt="React Logo" width='100' height={100} />
@@ -14,9 +17,7 @@ export default function Main() {
       <h1 className="main-title">React Essentials</h1>
 
       <p className="main-para">
-        {/* Here we are getting dynamic random Name from reactDescription array and diplaying it */}
-        {reactDescription[genRandomInt(2)]} 
-        React concepts you will need for almost every app you are going to build!</p>
+        {description} React concepts you will need for almost every app you are going to build!</p>
     </div>
   )
 }
