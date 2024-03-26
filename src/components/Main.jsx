e2d62698d71463fd19e87259dc1f612b806a35aa
Praphul Kumar <praphulkumar2023@gmail.com>
2024-03-26 13:23:13 +0530

    Displaying dynamic content in Custom Component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,11 @@
 import reactLogo from '../assets/react.svg'
 
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 export default function Main() {
   return (
     <div className="main">
@@ -7,7 +13,10 @@ export default function Main() {
 
       <h1 className="main-title">React Essentials</h1>
 
-      <p className="main-para">Fundamental React concepts you will need for almost every app you are going to build!</p>
+      <p className="main-para">
+        {/* Here we are getting dynamic random Name from reactDescription array and diplaying it */}
+        {reactDescription[genRandomInt(2)]} 
+        React concepts you will need for almost every app you are going to build!</p>
     </div>
   )
 }
