f8d3d00fd78222361289d24ea8595ac5b695610d
Praphul Kumar <praphulkumar2023@gmail.com>
2024-03-26 13:05:46 +0530

    Creating Custom Component in react

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import reactLogo from '../assets/react.svg'
+
+export default function Main() {
+  return (
+    <div className="main">
+      <img src={reactLogo} alt="React Logo" width='100' height={100} />
+
+      <h1 className="main-title">React Essentials</h1>
+
+      <p className="main-para">Fundamental React concepts you will need for almost every app you are going to build!</p>
+    </div>
+  )
+}
