252cb1546898c52f035c1c07805c922f41fa75df
Praphul <praphulkumar2023@gmail.com>
2024-11-01 22:24:43 +0530

    Synced: Project with course starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import Main from './components/Main'
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function App() {
-
-  return (
-    <>
-      {/* Using Custom Component */}
-      <Main />
-    </>
-  )
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 
-export default App
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are going to build!
+      </p>
+    </header>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
